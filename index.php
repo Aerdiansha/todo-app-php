1c241ab3029461d269013d0138e24b81429709b0
Aerdiansha <ahmaderdiansha.9a@gmail.com>
2025-01-14 20:27:53 +0700

    4th commit

--- index.php
+++ index.php
@@ -1,32 +1,44 @@
-<?php 
+<?php
 $todos = []; // total array yang disiapkan
-if(file_exists('todos.txt')) { // jika file todos.txt ada
+if (file_exists('todos.txt')) { // jika file todos.txt ada
     // maka membaca file todos.txt
     $file = file_get_contents('todos.txt');  // membaca file todos.txt
     $todos = unserialize($file); // mengubah format serialize ke array
-};
+}
+;
 
 
 // jika ditemukan data todo melalui method post
-if(isset($_POST['todo'])) {
+if (isset($_POST['todo'])) {
     $data = $_POST['todo']; // mengambil data todo pada form
     $todos[] = [
         'todo' => $data,
         'status' => 0
     ];
     file_put_contents('todos.txt', serialize($todos));
+    header('location: index.php');
 }
+
+if (isset($_GET['status'])) {
+    $todos[$_GET['key']]['status'] = $_GET['status'];
+    file_put_contents('todos.txt', serialize($todos));
+    header('location: index.php');
+}
+
+print_r($todos);
 
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To Do App</title>
 </head>
+
 <body>
     <h1>To Do App</h1>
 
@@ -37,13 +49,25 @@ if(isset($_POST['todo'])) {
     </form>
 
     <ul>
-        <?php foreach($todos as $key => $value): ?>
-        <li>
-            <input type="checkbox" name="todo">
-            <label><?= $value['todo']; ?></label>
-            <a href="#">Hapus</a>
-        </li>
+        <?php foreach ($todos as $key => $value): ?>
+            <li>
+                <input type="checkbox" name="todo"
+                    onclick="window.location.href = 'index.php?status=<?php ($value['status'] == 1 ? 0 : 1) ?>&key=<?= $key; ?>'"
+                    <?php if ($value['status'] == 1)
+                        echo 'checked'; ?>>
+                <label>
+                    <?php
+                    if ($value['status'] == 1) {
+                        echo '<del>' . $value['todo'] . '</del>';
+                    } else {
+                        echo $value['todo'];
+                    }
+                    ?>
+                </label>
+                <a href="#">Hapus</a>
+            </li>
         <?php endforeach; ?>
     </ul>
 </body>
+
 </html>
