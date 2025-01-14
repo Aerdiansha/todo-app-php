9d4b7bb21d1df3034290db49b733fd5f27cd0727
Aerdiansha <ahmaderdiansha.9a@gmail.com>
2025-01-14 19:45:05 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To Do App</title>
+</head>
+<body>
+    <h1>To Do App</h1>
+
+    <form action="">
+        <label for="">Apa kegiatanmu hari ini?</label>
+        <input type="text" name="todo">
+        <button type="submit">Simpan</button>
+    </form>
+
+    <ul>
+        <li>
+            <input type="checkbox" name="todo">
+            <label>Todo 1</label>
+            <a href="#">Hapus</a>
+        </li>
+        <li>
+            <input type="checkbox" name="todo">
+            <label>Todo 1</label>
+            <a href="#">Hapus</a>
+        </li>
+        <li>
+            <input type="checkbox" name="todo">
+            <label>Todo 1</label>
+            <a href="#">Hapus</a>
+        </li>
+    </ul>
+</body>
+</html>
